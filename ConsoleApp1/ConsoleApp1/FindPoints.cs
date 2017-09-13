281e6e18d0012c81c5a5c054b4e8e9f85412c576
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-14 00:23:16 +0300

    FindAllPointsMethoIsReady

--- ConsoleApp1/ConsoleApp1/FindPoints.cs
+++ ConsoleApp1/ConsoleApp1/FindPoints.cs
@@ -20,34 +20,40 @@ namespace ConsoleApp1
             get;
         }
 
+        public List<MyPoint> PointsList { get => pointsList; }
+
         public FindPoints(MainRectangle rect, int rectCount)
         {
             this.rectCount = rectCount;
             delta = CalcDelta(rect);
+            FindAllPoints(rect);
+
         }
 
         private double CalcDelta(MainRectangle rect) => Sqrt(rect.RectangleHeight * rect.RectangleWidth / rectCount);
 
+
         private void FindAllPoints(MainRectangle rect)
         {
+            pointsList = new List<MyPoint>();
+
             int firstForCount = (int)(rect.RectangleWidth / delta);
             int secondForCount= (int)(rect.RectangleHeight / delta);
 
             double x = rect.LeftBottom.X;
             double y = rect.LeftBottom.Y;
-
+         
 
             for (int i = 0; i < secondForCount; i++)
             {
                 for (int j = 0; j < firstForCount; j++)
                 {
-                   
-
+                    this.pointsList.Add(new MyPoint(x, y));
+                    x += delta;
                 }
-
+                y += delta;
             }
-
-
+      
         }
        
 
