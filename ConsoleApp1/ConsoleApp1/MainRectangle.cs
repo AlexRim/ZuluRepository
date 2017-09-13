fe052da1ff3a7727b18e3f69431d6fb8a005487a
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-13 22:42:16 +0300

    Commit 3 add some properties

--- ConsoleApp1/ConsoleApp1/MainRectangle.cs
+++ ConsoleApp1/ConsoleApp1/MainRectangle.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Math;
 
 namespace ConsoleApp1
 {
     class MainRectangle
     {
+        private MyPoint leftTop;
+
+        private MyPoint leftBottom;
+
+        private MyPoint rightTop;
+
+        private MyPoint rightBottom;
+
+        public double RectangleHeight => Sqrt(Pow(leftTop.Y - leftBottom.Y, 2) + Pow(leftTop.X - leftBottom.X, 2));
+        public double RectangleWidth => Sqrt(Pow(leftBottom.X - rightBottom.X, 2) + Pow(leftBottom.Y - rightBottom.Y,2));
+            
+
+
+        public MainRectangle(MyPoint lt, MyPoint lb, MyPoint rt, MyPoint rb)
+        {
+            this.leftTop = lt;
+            this.rightBottom = rb;
+            this.leftBottom = lb;
+            this.rightTop = rt;
+        }
+
+        public override string ToString()
+        {
+            return leftBottom.ToString() + " " + leftTop.ToString()+ " "+rightBottom.ToString()+" "+rightTop.ToString() ;
+        }
+
+
+
     }
 }
--- ConsoleApp1/ConsoleApp1/MyPoint.cs
+++ ConsoleApp1/ConsoleApp1/MyPoint.cs
@@ -8,5 +8,25 @@ namespace ConsoleApp1
 {
     class MyPoint
     {
+        private double x;
+        private double y;
+
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+
+         public MyPoint(){  }
+
+        public MyPoint(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+
+        public override string ToString()
+        {
+            return "X="+X+" "+"Y="+Y;
+        }
+
     }
 }
