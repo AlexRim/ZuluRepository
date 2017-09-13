6cb1330bdd755f27e1ed24937dedf4a12dfd721b
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-14 00:37:38 +0300

    almost rady will watch tomorrow

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var lt = new MyPoint(0, 10);
+            var lb = new MyPoint(0, 0);
+            var rt = new MyPoint(10,10);
+            var rb = new MyPoint(10, 0);
+
+            var rect = new MainRectangle(lt, lb, rt, rb);
+
+            var findPoints = new FindPoints(rect, 100);
+            var list = findPoints.PointsList;
+
+            foreach(var i in list)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.ReadKey();
         }
     }
 }
